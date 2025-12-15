73372732865bd4505130b5de4775ac2e74256e92
Sanjeet Balamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-12-15 21:57:06 +0530

    Update DoomMCClient.java

--- src/main/java/com/netherairtune/doommc/DoomMCClient.java
+++ src/main/java/com/netherairtune/doommc/DoomMCClient.java
@@ -1,21 +1,27 @@
 package com.netherairtune.doommc;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextColor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DoomMCClient implements ClientModInitializer {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger("DoomMC");
+
     @Override
     public void onInitializeClient() {
-        MinecraftClient client = MinecraftClient.getInstance();
-        if (client.player != null) {
-            Text message = Text.literal("Thanks for installing ")
-                    .append(Text.literal("DoomMC").styled(style -> style.withColor(TextColor.fromRgb(0xFF0000)))) // Red
-                    .append(Text.literal("! Have fun!").styled(style -> style.withColor(TextColor.fromRgb(0x00FF00)))); // Green
+        LOGGER.info("DoomMC client initializing");
 
-            client.player.sendMessage(message, false);
-        }
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            client.execute(() -> {
+                if (client.player != null) {
+                    Text message = Text.literal("Thanks for installing ").append(Text.literal("DoomMC").styled(style -> style.withColor(TextColor.fromRgb(0xFF0000)))).append(Text.literal("! Have fun!").styled(style -> style.withColor(TextColor.fromRgb(0x00FF00))));
+                    client.player.sendMessage(message, false);
+                }
+            });
+        });
     }
 }
