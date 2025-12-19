5f94c851052af6607cd43298c1866424cf35c3d7
SanjeetBalamurugan <72962798+SanjeetBalamurugan@users.noreply.github.com>
2025-12-19 23:08:47 +0530

    lets see

---
+++ native/DoomJNI.c
@@ -0,0 +1,44 @@
+#include <jni.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../DOOM/linuxdoom-1.10/doom_lib.h"
+
+static uint32_t framebuffer[SCREENWIDTH * SCREENHEIGHT];
+
+JNIEXPORT void JNICALL Java_com_netherairtune_doommc_DoomJNI_doomInit
+  (JNIEnv *env, jclass clazz, jobjectArray jargs) {
+    int argc = (*env)->GetArrayLength(env, jargs);
+    char **argv = malloc(sizeof(char*) * argc);
+    for (int i = 0; i < argc; i++) {
+        jstring str = (jstring)(*env)->GetObjectArrayElement(env, jargs, i);
+        const char *utf = (*env)->GetStringUTFChars(env, str, 0);
+        argv[i] = strdup(utf);
+        (*env)->ReleaseStringUTFChars(env, str, utf);
+    }
+    DOOM_Init(argc, argv);
+    for (int i = 0; i < argc; i++) free(argv[i]);
+    free(argv);
+}
+
+JNIEXPORT void JNICALL Java_com_netherairtune_doommc_DoomJNI_doomStep
+  (JNIEnv *env, jclass clazz) {
+    DOOM_RunTic();
+    memcpy(framebuffer, DOOM_GetScreenBuffer(), SCREENWIDTH * SCREENHEIGHT * sizeof(uint32_t));
+}
+
+JNIEXPORT jintArray JNICALL Java_com_netherairtune_doommc_DoomJNI_getFramebuffer
+  (JNIEnv *env, jclass clazz) {
+    jintArray out = (*env)->NewIntArray(env, SCREENWIDTH * SCREENHEIGHT);
+    (*env)->SetIntArrayRegion(env, out, 0, SCREENWIDTH * SCREENHEIGHT, (const jint*) framebuffer);
+    return out;
+}
+
+JNIEXPORT jint JNICALL Java_com_netherairtune_doommc_DoomJNI_getWidth
+  (JNIEnv *env, jclass clazz) {
+    return SCREENWIDTH;
+}
+
+JNIEXPORT jint JNICALL Java_com_netherairtune_doommc_DoomJNI_getHeight
+  (JNIEnv *env, jclass clazz) {
+    return SCREENHEIGHT;
+}
